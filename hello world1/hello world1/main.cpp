6e2822740fc6f6ce6ebd3254cce38eb29af39945
kimtaein <gimtaein288@gmail.com>
2025-09-15 15:55:35 +0900

    현장문제
    현장문제

--- hello world1/hello world1/main.cpp
+++ hello world1/hello world1/main.cpp
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
-int main()
-{
-	printf("hello!\n");
-	return 0;
+int main(void) {
+    char string[] = "Taein";
+    char string1[] = "T";
+    int decimal = 21;
+    unsigned int unsigned_decimal = 67890;
+    float floating_point = 3.141592;
+    char character = 'A';
+    
+
+    printf("Name : \t%s\n", string);
+    printf("Initial : \t%s\n", string1);
+    printf("Age : \t%d\n", decimal);
+    printf("부호 없는 정수(unsigned int): \t%u\n", unsigned_decimal);
+    printf("실수(float): \t%f\n", floating_point);
+    printf("문자(char): \t%c\n", character);
+
+    return 0;
 }
--- helloworld/helloworld/main.cpp
+++ helloworld/helloworld/main.cpp
@@ -2,6 +2,13 @@
 
 int main()
 {
-	printf("Hello, World!\n");
+	printf("=== basic Constans ===\n");
+	printf("Name    : Taein \n");
+	printf("Initial : T \n");
+	printf("Age     : 21 \n");
+	printf("Score   : 99.9000 \n");
+	printf("Count   : 30000000000 \n");
+	
+	
 	return 0;
 }
