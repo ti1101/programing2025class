814cbe4242d662ecc25c5dd17f26c9c5fbd36e60
kimtaein <gimtaein288@gmail.com>
2025-09-22 15:31:22 +0900

    현장연습문제#1
    현장연습문제#1

--- helloworld/helloworld/main.cpp
+++ helloworld/helloworld/main.cpp
@@ -3,11 +3,11 @@
 int main()
 {
 	printf("=== Basic Constans === \n\n");
-	printf("Name	: Taein \n\n");
-	printf("Initial	: T \n\n");
+	printf("Name\t: Taein \n\n");
+	printf("Initial\t: T \n\n");
 	printf("Age : 21 \n\n");
-	printf("Score	: 93.500000 \n\n");
-	printf("Count	: 3000000000 \n\n");
+	printf("Score\t: 93.500000 \n\n");
+	printf("Count\t: 3000000000 \n\n");
 	
 	
 	return 0;
