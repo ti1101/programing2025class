eee9abf5ae1b5f2491b5afd8ca2688b308b126c7
ti1101 <gimtaein288@gmail.com>
2025-09-29 16:46:54 +0900

    scanf
    scanf

---
+++ 929/929/1.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+#include <stdio.h>
+
+int main(void)
+{
+	int nAge = 99;
+	float fHeight = 999.0f;
+	double dWeight = 999.0;
+
+	char chTemp = ' ';
+	char chString[100] = " ";
+
+	//printf("Input your information(Age, Height, Weight):\n");
+	//scanf_s("%d%f%lf", &nAge, &fHeight, &dWeight);
+	printf("Input your alphabet and word:\n");
+	int nResScanf = scanf("%c%s", &chTemp, chString);
+	
+	printf("Output:%d\n", nResScanf);
+	//printf("(Age:%d)(Height:%.2f)(Weight:%.1lf)\n", nAge, fHeight, dWeight);
+	printf("(Alphabet:%c)(Word:%s)\n", chTemp, chString);
+
+	return 0;
+}
